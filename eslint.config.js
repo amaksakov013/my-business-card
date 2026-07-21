44d506221e8c9cccde78aeed428d30d36395b33c
amaksakov013 <157719412+amaksakov013@users.noreply.github.com>
2026-07-21 10:32:24 +0300

    Create Betonina one-page business card

---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, reactHooks.configs['recommended-latest'], reactRefresh.configs.vite, { ignores: ['dist'] })
