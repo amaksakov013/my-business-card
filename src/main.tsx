9e6a3c17615eddf05ea4bfe63257626755204d9f
amaksakov013 <157719412+amaksakov013@users.noreply.github.com>
2026-07-21 14:06:03 +0300

    Load compact responsive layout styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './image-fixes.css'
+import './compact-layout.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
